e5a6b42eba64ceaa3814ed96e4252ad7a5bb317e
DESKTOP-H9T1FUJ\lockt <coverfrog@gmail.com>
2025-07-27 21:34:45 +0900

    Spawner 추가

--- Assets/CCard/Runtime/Deck/CardDeck.cs
+++ Assets/CCard/Runtime/Deck/CardDeck.cs
@@ -9,71 +9,16 @@ namespace Cf.CCard
 {
     public class CardDeck : MonoBehaviour
     {
-        [Header("Addressable")]
-        [SerializeField] private AssetReference mCardReference;
-        
         [Header("Func")]
         [SerializeField] private CardStack mCardStackFunc;
         
-        [Header("Debug View")]
-        [SerializeField] private Card mCardPrefab;
-        [SerializeField] private List<Card> mCardsList = new List<Card>();
+        private Stack<Card> _mCardsStack = new Stack<Card>();
 
-        public void Init(Action onComplete)
+        public void Stack(Card card)
         {
-            StartCoroutine(CoInit(onComplete));
-        }
-
-        private IEnumerator CoInit(Action onComplete)
-        {
-            const int initTarget = 1;
-
-            int initCount = 0;
+            mCardStackFunc.Stack();
             
-            InitPrefab(() =>
-            {
-                initCount++;
-            });
-            
-            while (initCount < initTarget) yield return null;
-            
-            onComplete?.Invoke();
-        }
-
-        private void InitPrefab(Action onComplete)
-        {
-            bool isExist = mCardReference != null && mCardReference.RuntimeKeyIsValid();
-
-            if (!isExist)
-            {
-#if UNITY_EDITOR
-                Debug.Assert(false, "CardReference is not valid");
-#endif
-                return;
-            }
-
-            Addressables.LoadAssetAsync<GameObject>(mCardReference).Completed += op =>
-            {
-                if (op.Status != AsyncOperationStatus.Succeeded)
-                {
-#if UNITY_EDITOR
-                    Debug.Assert(false, "CardReference is not valid");
-#endif
-                    return;
-                }
-
-                if (op.Result.TryGetComponent(out mCardPrefab))
-                {
-                    onComplete?.Invoke();
-                }
-
-                else
-                {
-#if UNITY_EDITOR
-                    Debug.Assert(false, "CardReference is not valid");
-#endif
-                }
-            };
+            _mCardsStack.Push(card);
         }
     }
 }
---
+++ Assets/CCard/Runtime/Spawner/CardSpawner.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Cf.CCard
+{
+    public class CardSpawner : MonoBehaviour
+    {
+        
+    }
+}
